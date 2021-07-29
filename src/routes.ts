3aaa013523833e0f36e27c9d25c82db64f137bd8
Erico Almeida <erico.silva@invillia.com>
2021-07-29 06:42:40 -0400

    fixup: routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({
-    ok: true
-  });
+  return response.send('<h1>Full Cycle Rocks!</h1>');
 });
 
 export { routes }
